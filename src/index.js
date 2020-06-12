720b4e762b37f1d37d212e091469f9d9fdc50cb0
Israel Tekahun <israelheaven@yahoo.com>
2020-06-12 20:50:37 +0200

    unordered list test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<h1>test</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <ul>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
