35694930a31cabecec4c981e09ddd1c8658393f6
Israel Tekahun <israelheaven@yahoo.com>
2020-06-13 18:13:29 +0200

    test styling components checklist

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <main>this is my Main</main>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1"> I have a bike</label>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1"> I have a bike</label>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1"> I have a bike</label>
+      <br></br>
+    </div>
+  );
+}
+
+export default Main;
