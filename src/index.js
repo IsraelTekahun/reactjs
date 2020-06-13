3d0e525ff6faa66a657ef4498bf6b7dd27f1fba7
Israel Tekahun <israelheaven@yahoo.com>
2020-06-13 14:01:20 +0200

    ul in func

--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <div>
-    <ul>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+function MyInfo() {
+  return (
+    <div>
+      <h1>Israel Tekahun</h1>
+      <p>ሰላም፣ እስራኤል ነኝ፡፡ እንኳን ደህና መጡ!</p>
+      <p>Below are the places I would like to visit</p>
+      <ul>
+        <li>Ethiopia</li>
+        <li>Ethiopia</li>
+        <li>Ethiopia</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
