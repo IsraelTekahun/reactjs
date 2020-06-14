5e30ade648e095c7fe1fbf2b187fd5fb6c24674b
Israel Tekahun <israelheaven@yahoo.com>
2020-06-14 14:45:47 +0200

    conditional styling and inline styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,26 @@
 import React from "react";
 
+const styles = {
+  fontSize: 10,
+  color: "green",
+};
+
 function Nav() {
   let time = "morning";
   if (new Date().getHours() >= 17) {
     time = "night";
+    styles.color = "red";
   } else {
     time = "afternoon";
+    styles.color = "green";
   }
 
   return (
     <header>
       <h3 className="navbar">Good {time}</h3>
+      <h3 className="navbar" style={styles}>
+        Good {time}
+      </h3>
     </header>
   );
 }
