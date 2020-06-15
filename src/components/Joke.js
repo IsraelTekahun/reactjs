574b6dd288af03e4aabc6ef86e6c978824f8f6ff
israel <israelheaven@yahoo.com>
2020-06-15 17:53:53 +0200

    more styling examples

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -12,10 +12,21 @@ function App(props) {
   //1. <p style={{ display: props.joke.answer ? "block" : "none" }}> Answer: {ans}</p>
   //2. <p style={{ display: !props.joke.answer && "none" }}> Answer: {ans}</p>
 
-  return (
+  /*return (
     <div>
       <p>question: {props.joke.question}</p>
       <p style={{ display: !props.joke.answer && "none" }}> Answer: {ans}</p>
+      <hr />
+    </div>
+  );*/
+
+  //thid way and a little more styling of question that does not have answer
+  return (
+    <div>
+      <p style={{ color: !props.joke.answer && "green" }}>
+        question: {props.joke.question}
+      </p>
+      <p style={{ display: !props.joke.answer && "none" }}>Answer: {ans}</p>
       <hr />
     </div>
   );
