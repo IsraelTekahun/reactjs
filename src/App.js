9d71c346278ef65d72ecbce9bc7fbe83e5ce2ba9
israel <israelheaven@yahoo.com>
2020-06-15 21:44:21 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import Joke from "./components/Joke";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Joke
+        joke={{
+          question: "What two things can you never eat for breakfast?",
+        }}
+      />
+      <Joke
+        joke={{
+          question: "What gets wetter the more it dries?",
+          answer: "A towel.",
+        }}
+      />
+      <Joke
+        joke={{
+          question:
+            "What never asks a question but gets answered all the time?",
+          answer: "Your cellphone.",
+        }}
+      />
+      <Joke
+        joke={{
+          question: "What goes up but never ever comes down?",
+          answer: "Your age.",
+        }}
+      />
+      <Joke
+        joke={{
+          question:
+            "A girl fell off a 50-foot ladder but didn’t get hurt. How come?",
+          answer: "She fell off the bottom rung.",
+        }}
+      />
     </div>
   );
 }
