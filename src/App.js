b1cf76395c6db95d8aef1835781814bb5d6fb9ed
israel <israelheaven@yahoo.com>
2020-06-16 19:39:03 +0200

    online example map components: send entier object to component

--- src/App.js
+++ src/App.js
@@ -6,31 +6,25 @@ product in the list.
 Make sure to use the array's `.map()` method to create these components, and 
 don't forget to pass a `key` prop to it to avoid the warning.
 */
+/* 
+Given a list of products (as an array of objects, as seen in productsData.js)
+render a <Product /> component (which you'll also need to create) for each
+product in the list.
+
+Make sure to use the array's `.map()` method to create these components, and 
+don't forget to pass a `key` prop to it to avoid the warning.
+*/
 
 import React from "react";
-import vschoolProducts from "./data2/vschoolProducts";
-import Product from "./components/Product";
+import Product from "./Product";
+import productsData from "./vschoolProducts";
 
 function App() {
-  console.log(vschoolProducts[0]);
-
-  /*let p2 = vschoolProducts.map((v) => {
-    console.log(v.id);
-    //return v.id;
-  });*/
-
-  const jokeComponents = vschoolProducts.map((joke) => console.log(joke));
-
-  const p = vschoolProducts.map((v) => (
-    <Product
-      key={v.id}
-      name={v.name}
-      price={v.price}
-      description={v.description}
-    />
+  const productComponents = productsData.map((item) => (
+    <Product key={item.id} product={item} />
   ));
 
-  return <div>{p}</div>;
+  return <div>{productComponents}</div>;
 }
 
 export default App;
