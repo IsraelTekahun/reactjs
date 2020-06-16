89d6d8253780b0b430f4ab69d17f3c9afad147fe
israel <israelheaven@yahoo.com>
2020-06-16 19:49:22 +0200

    todolist

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <footer>
+      <h3>this is my nav</h3>
+    </footer>
+  );
+}
+
+export default Nav;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoItem() {
+  return (
+    <div>
+      <input type="checkbox"></input>
+      <p>Place holder text here</p>
+    </div>
+  );
+}
+
+export default TodoItem;
