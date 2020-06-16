62675b0ba37a0255ddaf33bdce3c538138ed3685
israel <israelheaven@yahoo.com>
2020-06-16 20:45:19 +0200

    todo checkbox dynamic ...

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
+/*
+Let's practice props and mapping components on our todo list app!
 
-import Nav from "./components/Nav.js";
-import Main from "./components/Main.js";
-import Footer from "./components/Footer.js";
+I've created a js file with some todos data in it, which I'm imported into this file. (Normally this data would come from an API call, not a local file). 
+
+Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
+*/
+
+import React from "react";
+import TodoItem from "./components/TodoItem";
+import todosData from "./data/todosData";
 
 function App() {
-  return (
-    <div>
-      <Nav />
-      <Main />
-      <Footer />
-    </div>
-  );
+  console.log(todosData);
+
+  let td = todosData.map((td) => <TodoItem key={td.id} todo={td} />);
+
+  return <div>{td}</div>;
 }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function TodoItem() {
+function TodoItem(props) {
+  let c = "completed";
+  if (!props.todo.completed) c = "not completed";
+
   return (
     <div>
-      <input type="checkbox"></input>
-      <p>Place holder text here</p>
+      <input type="checkbox" checked={props.todo.completed} />
+      <p>{props.todo.text}</p>
+      <p>{c}</p>
     </div>
   );
 }
