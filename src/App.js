fe2f5a68068221ff64c19feb97e20ed8aa2e3053
Israel Tekahun <israelheaven@yahoo.com>
2020-06-18 22:23:50 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from "react";
 
 function App() {
+  /* three ways */
+
+  //#1
+  /*return (
+    <div className="App">
+      <button
+        onClick={function () {
+          console.log("clícked");
+        }}
+      >
+        test
+      </button>
+    </div>
+  );*/
+
+  //#2
+  /*return (
+    <div className="App">
+      <button
+        onClick={() => {
+          console.log("clícked");
+        }}
+      >
+        test
+      </button>
+    </div>
+  );*/
+
+  //#3
+  function handleClick() {
+    console.log("clicked");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">test</header>
+      <img
+        onMouseOver={() => console.log("Hovered!")}
+        src="https://www.fillmurray.com/200/100"
+      />
+      <br />
+      <br />
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
