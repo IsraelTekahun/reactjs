3c103d9617ccf1c8fdf23c08d2f135a1656404d8
Israel Tekahun <israelheaven@yahoo.com>
2020-06-18 17:42:32 +0200

    class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Greeting from "./components/Greeting";
 
-function App() {
+// #1
+/*function App() {
   return (
-    <div className="App">
-      <header className="App-header">test</header>
+    <div>
+      <Header />
+      <Greeting />
     </div>
   );
-}
+} 
+export default App;*/
 
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header username="test" />
+        <Greeting />
+      </div>
+    );
+  }
+}
 export default App;
