b6ee7cf9ee9de4abe6dbe0c9099fc02ecd00f607
Israel Tekahun <israelheaven@yahoo.com>
2020-06-19 14:24:36 +0200

    ...

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,45 +1,16 @@
 import React from "react";
 
-/*function TodoItem(props) {
-  let c = "completed";
-  if (!props.todo.completed) c = "not completed";
-
+function TodoItem(props) {
   return (
-    <div>
-      <input type="checkbox" checked={props.todo.completed} />
-      <p>{props.todo.text}</p>
-      <p>{c}</p>
-      <hr />
+    <div className="todo-item">
+      <input
+        type="checkbox"
+        checked={props.item.completed}
+        onChange={() => props.handleChange(props.item.id)}
+      />
+      <p>{props.item.text}</p>
     </div>
   );
-}
-
-export default TodoItem;*/
-
-class TodoItem extends React.Component {
-  constructor(props) {
-    console.log(props.todo.completed);
-
-    super();
-    this.state = {
-      text: props.todo.text,
-      completed: props.todo.completed,
-    };
-  }
-
-  render() {
-    let c = "completed";
-    if (!this.state.completed) c = "not completed";
-
-    return (
-      <div>
-        <input type="checkbox" checked={this.state.completed} />
-        <p>{this.state.text}</p>
-        <p>{c}</p>
-        <hr />
-      </div>
-    );
-  }
 }
 
 export default TodoItem;
